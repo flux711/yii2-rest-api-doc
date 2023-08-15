61475338b2e7b76c7d78a2d50f31328c48a2a5ba
Frederik Lux <f.lux@phytec.de>
2023-08-15 15:16:26 +0200

    change description behavior to show multiple sentences in expanded view description and the first sentence at collapsed view
    Signed-off-by: Frederik Lux <f.lux@phytec.de>

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -2,145 +2,192 @@
 
 namespace flux711\yii2\rest_api_doc\controllers;
 
+use flux711\yii2\rest_api_doc\ModuleAsset;
+use ReflectionClass;
+use Yii;
+use yii\base\Controller;
+use yii\base\Exception;
 use yii\helpers\BaseInflector;
+use yii\rest\UrlRule;
 
-class DefaultController extends \yii\base\Controller
+class DefaultController extends Controller
 {
 
 	public function init()
 	{
 		$view = $this->getView();
-		\flux711\yii2\rest_api_doc\ModuleAsset::register($view);
+		ModuleAsset::register($view);
 		parent::init();
-    }
+	}
 
-    public function actionIndex()
-    {
-        $rules = [];
-        foreach (\Yii::$app->urlManager->rules as $urlRule) {
-            if ($urlRule instanceof \yii\rest\UrlRule) {
-                $entity = [];
-                $controllerName = current($urlRule->controller);
-				$entity['title'] = str_replace(['/'], '_', ucfirst($controllerName));
-				$urlRuleReflection = new \ReflectionClass($urlRule);
-                $rulesObject = $urlRuleReflection->getProperty('rules');
-                $rulesObject->setAccessible(true);
-                $generatedRules = $rulesObject->getValue($urlRule);
+	public function actionIndex()
+	{
+		$rules = [];
+		foreach(Yii::$app->urlManager->rules as $urlRule) {
+			if ($urlRule instanceof UrlRule) {
+				$entity = [];
+				$controllerName = current($urlRule->controller);
 
-                $entity['rules'] = $this->_processRules($generatedRules[$controllerName]);
+				if (strpos($controllerName, '/')) {
+					$version = substr($controllerName, 0, strpos($controllerName, '/'));
+					$controller_title = substr($controllerName, strpos($controllerName, '/') + 1);
+					$entity['title'] = ucfirst($controller_title).'_'.ucfirst($version);
+				} else {
+					$entity['title'] = ucfirst(substr($controllerName, strpos($controllerName, '/')));
+				}
 
-                $rules[] = $entity;
-            }
-        }
-        return $this->render('index', [
-                'rules' => $rules,
-        ]);
-    }
+				$urlRuleReflection = new ReflectionClass($urlRule);
+				$rulesObject = $urlRuleReflection->getProperty('rules');
+				$rulesObject->setAccessible(true);
+				$generatedRules = $rulesObject->getValue($urlRule);
+				$entity['rules'] = $this->_processRules($generatedRules[$controllerName]);
+				$rules[] = $entity;
+			}
+		}
 
-    function _processRules($generatedRules)
-    {
-        $rules = [];
+		usort($rules, function($item_a, $item_b) {
+			return strcmp($item_a['title'], $item_b['title']);
+		});
 
-        foreach ($generatedRules as $generatedRule) {
-            $reflectionObject = new \ReflectionClass($generatedRule);
-            $templateObject = $reflectionObject->getProperty('_template');
-            $templateObject->setAccessible(true);
-            if (empty($generatedRule->verb)) {
-                continue;
-            }
-            $rule = [];
-            $rule['url'] = str_replace(['<', '>'], ['{', '}'], rtrim($templateObject->getValue($generatedRule), '/'));
-            $rule['method'] = current($generatedRule->verb);
-            preg_match_all('/\{[^}]*\}/', $rule['url'], $matched);
+		return $this->render('index', [
+			'rules' => $rules,
+		]);
+	}
 
-            $params = [];
-            if (!empty($matched[0])) {
-                foreach ($matched[0] as $key) {
-                    $name = str_replace(['{', '}'], '', $key);
-                    $params[] = [
-                        'key' => $key,
-                        'name' => $name,
-                        'title' => $name == 'id' ? 'ID' : ucfirst(str_replace('_', ' ', $name)),
-                    ];
-                }
-            }
+	function _processRules($generatedRules)
+	{
+		$rules = [];
+		foreach($generatedRules as $generatedRule) {
+			$reflectionObject = new ReflectionClass($generatedRule);
+			$templateObject = $reflectionObject->getProperty('_template');
+			$templateObject->setAccessible(true);
+			if (empty($generatedRule->verb)) {
+				continue;
+			}
+			$rule = [];
+			$rule['url'] = str_replace(['<', '>'], ['{', '}'], rtrim($templateObject->getValue($generatedRule), '/'));
+			$rule['method'] = current($generatedRule->verb);
+			preg_match_all('/\{[^}]*\}/', $rule['url'], $matched);
 
-            $rule['params'] = $params;
+			$params = [];
+			if (!empty($matched[0])) {
+				foreach($matched[0] as $key) {
+					$name = str_replace(['{', '}'], '', $key);
+					$params[] = [
+						'key' => $key,
+						'name' => $name,
+						'title' => $name == 'id' ? 'ID' : ucfirst(str_replace('_', ' ', $name)),
+					];
+				}
+			}
 
-            list($controller, $actionID) = \Yii::$app->createController($generatedRule->route);
+			$rule['params'] = $params;
 
-            try {
-                $methodName = 'action' . BaseInflector::id2camel($actionID);
-                $controllerReflection = new \ReflectionClass($controller);
-                $methodInfo = $controllerReflection->getMethod($methodName);
-                $fieldsString = $this->_findString($methodInfo->getDocComment(), 'Rest Fields');
-                if ($fieldsString) {
-                    $fieldsOptions = explode('||', $fieldsString);
-                    foreach ($fieldsOptions as $fieldsOption) {
-                        eval('$rule[\'fields\'][] = ' . $fieldsOption . ';');
-                    }
-                }
-                $rule['filters'] = $this->_findElements($methodInfo->getDocComment(), 'Rest Filters');
-                $rule['expand'] = $this->_findElements($methodInfo->getDocComment(), 'Rest Expand');
-                $rule['description'] = $this->_findString($methodInfo->getDocComment(), 'Rest Description');
-            } catch (\Exception $ex) {
-                // Silence.
-            }
+			[$controller, $actionID] = \Yii::$app->createController($generatedRule->route);
 
-            if (!empty($rule['fields'])) {
-                $rule['fields'] = $this->_fieldsFlip($rule['fields']);
-            }
+			$rest_parameters = ['Rest Fields', 'Rest Filters', 'Rest Expand', 'Rest Description'];
+			$combined_rest_parameters = array_map(function($param) {
+				return $param.':';
+			}, $rest_parameters);
+			$combined_rest_parameters = implode('|', $combined_rest_parameters);
+			$pattern = "([\s\S]*?)(?=".$combined_rest_parameters."|\/|$)";
 
-            $rules[] = $rule;
-        }
+			try {
+				$methodName = 'action'.BaseInflector::id2camel($actionID);
+				$controllerReflection = new \ReflectionClass($controller);
+				$methodInfo = $controllerReflection->getMethod($methodName);
 
-        usort($rules, function($a, $b) {
-            return strcmp($a['url'], $b['url']);
-        });
+				$fieldsString = $this->_findString($methodInfo->getDocComment(), 'Rest Fields', $pattern);
+				if ($fieldsString['full']) {
+					$fieldsString = rtrim($fieldsString['full'], '.');
+					$fieldsOptions = explode('||', $fieldsString);
+					foreach($fieldsOptions as $fieldsOption) {
+						eval('$rule[\'fields\'][] = '.$fieldsOption.';');
+					}
+				}
 
-        return $rules;
-    }
+				$rule['filters'] = $this->_findElements($methodInfo->getDocComment(), 'Rest Filters', $pattern);
+				$rule['expand'] = $this->_findElements($methodInfo->getDocComment(), 'Rest Expand', $pattern);
+				$rule['description'] = $this->_findString($methodInfo->getDocComment(), 'Rest Description', $pattern);
+			} catch (Exception $e) {
+				// Silence
+			}
 
-    function _fieldsFlip($fields)
-    {
-        $flipped = [];
-        foreach ($fields as $key => $field) {
-            if (is_array($field)) {
-                $flipped[$key] = $this->_fieldsFlip($field);
-            } else {
-                if (substr($field, 0, 1) == '_') {
-                    $field = substr($field, 1);
-                }
-                $flipped[$field] = '';
-            }
-        }
-        return $flipped;
-    }
+			if (!empty($rule['fields'])) {
+				$rule['fields'] = $this->_fieldsFlip($rule['fields']);
+			}
 
-    function _findString($string, $title, $pattern = '[^.]*\.')
-    {
-        preg_match("/$title:$pattern/", str_replace('*', '', $string), $matched);
-        if (!empty($matched[0])) {
-            return trim(str_replace($title . ':', '', $matched[0]), ' .');
-        }
-    }
+			$rules[] = $rule;
+		}
 
-    function _findElements($string, $title, $pattern = '[^.]*\.')
-    {
-        $elementsString = $this->_findString($string, $title, $pattern);
-        $elements = [];
-        if ($elementsString) {
-            eval('$elements = ' . $elementsString . ';');
-        }
-        $finalElements = [];
-        if (!empty($elements)) {
-            foreach ($elements as $element) {
-                $finalElements[] = [
-                    'title' => ucfirst(str_replace('_', ' ', $element)),
-                    'key' => $element,
-                ];
-            }
-        }
-        return $finalElements;
-    }
+		usort($rules, function($a, $b) {
+			return strcmp($a['url'], $b['url']);
+		});
+
+		return $rules;
+	}
+
+	function _fieldsFlip($fields)
+	{
+		$flipped = [];
+		foreach($fields as $key => $field) {
+			if (is_array($field)) {
+				$flipped[$key] = $this->_fieldsFlip($field);
+			} else {
+				if (substr($field, 0, 1) == '_') {
+					$field = substr($field, 1);
+				}
+				$flipped[$field] = '';
+			}
+		}
+		return $flipped;
+	}
+
+	function _findString($string, $title, $pattern)
+	{
+		$full_description = "";
+		$pattern = "/".$title.":".$pattern."/";
+		preg_match($pattern, str_replace('*', '', $string), $matched);
+		if (!empty($matched[1])) {
+			$full_description = trim($matched[1]);
+		}
+
+		$short_description = "";
+		$extended_description = "";
+		if ($full_description) {
+			$description_sentences = explode('.', $full_description);
+			if (!empty($description_sentences)) {
+				$short_description = $description_sentences[0];
+				// Remove the first sentence from the array
+				array_shift($description_sentences);
+				$extended_description = implode('. ', $description_sentences);
+			}
+		}
+
+		return [
+			'short' => $short_description,
+			'extended' => $extended_description,
+			'full' => $full_description
+		];
+	}
+
+	function _findElements($string, $title, $pattern)
+	{
+		$elementsString = $this->_findString($string, $title, $pattern);
+		$elementsString = rtrim($elementsString['full'], '.');
+		$elements = [];
+		if ($elementsString) {
+			eval('$elements = '.$elementsString.';');
+		}
+		$finalElements = [];
+		if (!empty($elements)) {
+			foreach($elements as $element) {
+				$finalElements[] = [
+					'title' => ucfirst(str_replace('_', ' ', $element)),
+					'key' => $element,
+				];
+			}
+		}
+		return $finalElements;
+	}
 }
